df823471088ce0163bfb228789bd14f40d132096
Aaron Chan <aarchan108@gmail.com>
2025-07-17 22:18:27 -0700

    Use ArduinoJson

--- include/datalogging.h
+++ include/datalogging.h
@@ -1,16 +1,10 @@
 #pragma once
 #include <Arduino.h>
-#include <ArduinoJson.h>
-#include <LittleFS.h>
 
 
 struct Settings {
     float log_altitude_threshold;
-    // Add more settings as needed
 };
-
-extern Settings g_settings;
-extern uint32_t g_bootcount;
 
 void dataloggingInit();
 void dataloggingTask(void* pvParameters);
