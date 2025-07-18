79ebbea3971d54bd907f65966d27efe7dbe9d254
Aaron Chan <aarchan108@gmail.com>
2025-07-17 21:33:47 -0700

    Working compile with setup and loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,16 @@
 #include "gnss.h"
 #include "sensing.h"
 
-int main(void) {
+void setup() {
     Serial.begin(9600);
-
-    // Initialize
     sensingInit();
     dataloggingInit();
     gnssInit();
 
-    // Create tasks
     xTaskCreate(sensingTask, "Sensing Task", 2048, NULL, 1, NULL);
     xTaskCreate(dataloggingTask, "Datalogging Task", 2048, NULL, 1, NULL);
     xTaskCreate(gnssTask, "GNSS Task", 2048, NULL, 1, NULL);
+}
 
-    // Start scheduler
-    vTaskStartScheduler();
-
-    // Should never reach here
-    return 0;
+void loop() {
 }
