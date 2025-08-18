58d65c101642b8b0a3815639bdd88a5ec6195310
Aaron Chan <aarchan108@gmail.com>
2025-08-18 01:14:34 -0400

    Sensing execute function

--- src/sensing.cpp
+++ src/sensing.cpp
@@ -23,39 +23,43 @@ void sensingInit() {
     Serial.println("Sensors initialized");
 }
 
+void sensingExecute() {
+    ms5607.doBaro(true);
+    float ms5607Pressure = ms5607.pressure;
+    float ms5607Temperature = ms5607.temperature;
+    float ms5607Altitude = ms5607.altitude;
+    float hdcTemp = hdc2080.readTemp();
+    float hdcHum = hdc2080.readHumidity();
+
+    SensingData data;
+    data.humidity = hdcHum;
+    data.temperature = hdcTemp;
+    data.pressure = ms5607Pressure;
+    data.baroAltitude = ms5607Altitude;
+    xQueueOverwrite(sensingQueue, &data);
+
+    Serial.print("MS5607 Altitude: ");
+    Serial.print(ms5607Altitude);
+    Serial.print(" m, ");
+    Serial.print("Pressure: ");
+    Serial.print(ms5607Pressure);
+    Serial.print(" mbar, ");
+    Serial.print("Temp: ");
+    Serial.print(ms5607Temperature);
+    Serial.println(" C");
+
+    Serial.print("HDC2080 Temp: ");
+    Serial.print(hdcTemp);
+    Serial.print(" C, ");
+    Serial.print("Humidity: ");
+    Serial.print(hdcHum);
+    Serial.println(" %");
+}
+
 void sensingTask(void* pvParameters) {
     Serial.println("Sensing task started");
     while (1) {
-        ms5607.doBaro(true);
-        float ms5607Pressure = ms5607.pressure;
-        float ms5607Temperature = ms5607.temperature;
-        float ms5607Altitude = ms5607.altitude;
-        float hdcTemp = hdc2080.readTemp();
-        float hdcHum = hdc2080.readHumidity();
-
-        SensingData data;
-        data.humidity = hdcHum;
-        data.temperature = hdcTemp;
-        data.pressure = ms5607Pressure;
-        data.baroAltitude = ms5607Altitude;
-        xQueueOverwrite(sensingQueue, &data);
-
-        Serial.print("MS5607 Altitude: ");
-        Serial.print(ms5607Altitude);
-        Serial.print(" m, ");
-        Serial.print("Pressure: ");
-        Serial.print(ms5607Pressure);
-        Serial.print(" mbar, ");
-        Serial.print("Temp: ");
-        Serial.print(ms5607Temperature);
-        Serial.println(" C");
-
-        Serial.print("HDC2080 Temp: ");
-        Serial.print(hdcTemp);
-        Serial.print(" C, ");
-        Serial.print("Humidity: ");
-        Serial.print(hdcHum);
-        Serial.println(" %");
+        sensingExecute();
 
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
