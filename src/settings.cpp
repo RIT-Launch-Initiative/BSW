da0aa913fbbafdd0d2d7e187e93189a9ea71f3cd
Aaron Chan <aarchan108@gmail.com>
2025-10-13 03:15:41 -0400

    defn consts for defaults

--- src/settings.cpp
+++ src/settings.cpp
@@ -8,13 +8,27 @@
 static char SETTINGS_FILE[FILE_NAME_SIZE] = "settings.json";
 extern SdFs sd;
 
+enum SettingsConstants {
+    LOG_ALTITUDE_THRESHOLD_DEFAULT = 100, // meters
+    BARO_ALTITUDE_TOLERANCE_DEFAULT = 5,  // meters
+    GPS_ALTITUDE_TOLERANCE_DEFAULT = 5,   // meters
+}
+
+static void loadDefaultSettings(Settings& settings) {
+    settings.logAltitudeThresholdMeters = LOG_ALTITUDE_THRESHOLD_DEFAULT;
+    settings.baroAltitudeTolerance = BARO_ALTITUDE_TOLERANCE_DEFAULT;
+    settings.gpsAltitudeTolerance = GPS_ALTITUDE_TOLERANCE_DEFAULT;
+}
+
 void loadSettings(Settings& settings) {
     Serial.println("==============");
     Serial.println(" Load Settings ");
     Serial.println("==============");
-    
+
     JsonDocument doc;
     uint8_t buffer[256] = {0};
+
+    loadDefaultSettings(settings);
 
     if (!sd.exists(SETTINGS_FILE)) {
         Serial.println("Settings file not found, using default settings");
@@ -40,9 +54,9 @@ void loadSettings(Settings& settings) {
         Serial.print("Failed to parse settings file: ");
         Serial.println(error.c_str());
     } else {
-        settings.logAltitudeThresholdMeters = doc["logAltitudeThresholdMeters"] | 100.0f;
-        settings.baroAltitudeTolerance = doc["baroAltitudeTolerance"] | 5.0f;
-        settings.gpsAltitudeTolerance = doc["gpsAltitudeTolerance"] | 5.0f;
+        settings.logAltitudeThresholdMeters = doc["logAltitudeThresholdMeters"] | LOG_ALTITUDE_THRESHOLD_DEFAULT;
+        settings.baroAltitudeTolerance = doc["baroAltitudeTolerance"] | BARO_ALTITUDE_TOLERANCE_DEFAULT;
+        settings.gpsAltitudeTolerance = doc["gpsAltitudeTolerance"] | GPS_ALTITUDE_TOLERANCE_DEFAULT;
     }
 
     file.close();
